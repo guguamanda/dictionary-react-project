18ae6ab2c4000c45065561853df1dddd2ca36155
g-ndlovu <langathiwendlovu97@gmail.com>
2024-12-18 18:41:47 +0200

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiURL).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
