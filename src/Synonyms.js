d67f5def8a7f0c3bfb7a1e25d654a6442921d09a
g-ndlovu <langathiwendlovu97@gmail.com>
2024-12-19 21:12:46 +0200

    Added synonyms

---
+++ src/Synonyms.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Synonyms.css";
+
+export default function Synonyms(props) {
+  if (props.synonyms && props.synonyms.length > 0) {
+    return (
+      <div className="Synonyms">
+        <ul>
+          <strong>Similar:</strong>
+          <br />
+          {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
